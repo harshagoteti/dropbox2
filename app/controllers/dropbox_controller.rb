091eaa20175521bebbe05afb4303df96dfaf3b87
harshagoteti <goteti.sriharsha@gmail.com>
2013-06-06 15:33:49 +0530

    done commit

---
+++ app/controllers/dropbox_controller.rb
@@ -0,0 +1,4 @@
+class DropboxController < ApplicationController
+       def index 
+       end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dropbox::Application.config.secret_token = 'e7d034dffdccba9a047aa6c2ebce5c20171e05e834484adc1f948f779af2aacaa672082ac277f661706a95fe03f5d0e3f11384cc2f808b665f48f909e7c36046'
