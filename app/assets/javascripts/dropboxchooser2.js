c43489433986606dbd0b7768b760c320d9ad8d20
harshagoteti <goteti.sriharsha@gmail.com>
2013-06-25 11:15:16 +0530

    chooser button added

---
+++ app/assets/javascripts/dropboxchooser2.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="https://www.dropbox.com/static/api/1/dropins.js" id="dropboxjs" data-app-key="hyizkg1zb057ti8"></script>
--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -1,10 +1,7 @@
 class DropboxController < ApplicationController
        def index
-  render :action => "index" 
-    end
-def uploadFile
-redirect :to =>"sign_in"
-end
+  render :action => "index"
+      end
 def edit
 end
 def new
